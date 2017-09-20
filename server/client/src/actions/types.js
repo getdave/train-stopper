b5641c0ced98e7be8ad0b6f411ddb7a95e910c85
David Smith <david.smith@mrbandfriends.co.uk>
2017-09-20 08:07:45 +0100

    Remove unwanted files

--- server/client/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-
-// EXAMPLE
-export const FETCHING_EXAMPLES         = 'fetching_examples';
-export const FETCHING_EXAMPLES_SUCCESS = 'fetching_examples_success';
-export const FETCHING_EXAMPLES_FAILED  = 'fetching_stations_failed';
